32d8a3d494b47cf939d7758e0387ff93e7abfe37
BuildTools <beckadamtheartist@gmail.com>
2020-03-27 21:47:44 -0600

    Fix copy/paste bug, add background image feature!

---
+++ backgrounds/background-0.asm
@@ -0,0 +1,10 @@
+
+include 'include/tiformat.inc'
+include 'include/ez80.inc'
+
+format ti archived appvar "BOSBG000"
+
+db 'BOSBG',0,$FF,'TSunset',0,$FF,'Abeckadamtheinventor',0,0
+include 'gfx/BOSshell-background-0.asm'
+
+
---
+++ backgrounds/background-3.asm
@@ -0,0 +1,10 @@
+
+include 'include/tiformat.inc'
+include 'include/ez80.inc'
+
+format ti archived appvar "BOSBG003"
+
+db 'BOSBG',0,$FF,'TDeep Blue Snow',0,$FF,'Abeckadamtheinventor',0,0
+include 'gfx/BOSshell-background-Deep-Blue-Snow.asm'
+
+
